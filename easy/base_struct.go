0e02656f2035f36afedf78e92e70c702c40f4bfa
chens <chensheng@17paipai.cn>
2018-04-18 18:02:48 +0800

    update

--- easy/base_struct.go
+++ easy/base_struct.go
@@ -19,3 +19,41 @@ func (l *ListNode) string() {
 	}
 	fmt.Println(strings.Join(s, "->"))
 }
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func (t *TreeNode) preorderTraversal() {
+	root := t
+	fmt.Println(root.Val)
+	if root.Left != nil {
+		t.preorderTraversal()
+	}
+}
+
+func preorderTraversal(root *TreeNode) {
+	fmt.Print(root.Val, " ")
+	if root.Left == nil && root.Right == nil {
+		return
+	}
+	if root.Left == nil {
+		fmt.Print("null", " ")
+	} else {
+		preorderTraversal(root.Left)
+	}
+	if root.Right == nil {
+		fmt.Print("null", " ")
+	} else {
+		preorderTraversal(root.Right)
+	}
+}
+
+func main() {
+	t4 := &TreeNode{1, nil, nil}
+	//t3 := &TreeNode{2, nil, nil}
+	t2 := &TreeNode{3, nil, t4}
+	preorderTraversal(t2)
+}
