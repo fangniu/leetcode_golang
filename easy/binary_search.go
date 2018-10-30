50402a7790becfebd200959cf3c01cc987b8c4e9
chens <chensheng@17paipai.cn>
2018-10-30 17:31:40 +0800

    update

---
+++ easy/binary_search.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+//
+//
+//Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search
+// target in nums. If target exists, then return its index, otherwise return -1.
+//
+//
+//Example 1:
+//
+//Input: nums = [-1,0,3,5,9,12], target = 9
+//Output: 4
+//Explanation: 9 exists in nums and its index is 4
+//
+//Example 2:
+//
+//Input: nums = [-1,0,3,5,9,12], target = 2
+//Output: -1
+//Explanation: 2 does not exist in nums so return -1
+//
+//
+//Note:
+//
+//You may assume that all elements in nums are unique.
+//n will be in the range [1, 10000].
+//The value of each element in nums will be in the range [-9999, 9999].
+
+
+func search(nums []int, target int) int {
+	i := 0
+	j := len(nums) - 1
+	for i <= j {
+		m := i + (j-i)/2
+		n := nums[m]
+		if target == n {
+			return m
+		} else if target > n {
+			i = m + 1
+		} else if target < n {
+			j = m - 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(search([]int{1,2,3,4,5,6,7,8,9,10,11,12}, 12))
+}
